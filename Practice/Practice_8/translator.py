222e73cf18bb75d9512ae7c9bf06a357b7709194
Lucaschewitch <tsuyoiibakatoshii@gmail.com>
2025-03-04 19:03:19 +0300

    Add files via upload

---
+++ Practice/Practice_8/translator.py
@@ -0,0 +1,22 @@
+from deep_translator import GoogleTranslator
+import pymorphy2 as morphy
+
+class Translator:
+
+    def __init__(self):
+        self.morphy = morphy.MorphAnalyzer()
+        self.translator = GoogleTranslator()
+
+    def translate(self, word: str) -> str:
+        return self.translator.translate(word)
+
+    def word_tool(self):
+        return self.WordWrapper(self.morphy)
+
+    class WordWrapper:
+
+        def __init__(self, morphy_instance: morphy.MorphAnalyzer):
+            self.morphy = morphy_instance
+
+        def normalize(self, word: str) -> str:
+            return self.morphy.parse(word)[0].normal_form
